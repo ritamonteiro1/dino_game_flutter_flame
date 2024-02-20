d2cd3bfbe69692628cda77e2cf4100cd99fc55b9
m.rita.monteiro <rita.alves@mobills.com.br>
2024-02-20 12:40:11 -0300

    add flame libs

--- dependencies_src/lib/dependencies_src.dart
+++ dependencies_src/lib/dependencies_src.dart
@@ -1,7 +1,3 @@
 library dependencies_src;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/flame.dart';
---
+++ dependencies_src/lib/src/flame.dart
@@ -0,0 +1,2 @@
+export 'package:flame/flame.dart';
+export 'package:flame_audio/flame_audio.dart';

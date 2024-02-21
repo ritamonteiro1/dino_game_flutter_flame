88cb38b30045d5ac4661d3cd0159ed822664e234
m.rita.monteiro <rita.alves@mobills.com.br>
2024-02-21 10:52:34 -0300

    add methods to load assets on DinoGame

--- game/lib/main.dart
+++ game/lib/main.dart
@@ -1,7 +1,96 @@
-import 'package:flutter/cupertino.dart';
+import 'package:dependencies_src/dependencies_src.dart';
+import 'package:design_system/design_system.dart';
 import 'package:flutter/material.dart';
-import 'package:game/src/dino_app.dart';
+import 'package:game/src/system/dino_game.dart';
+import 'package:game/src/utils/overlay_builder_ids.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const DinoApp());
+}
+
+class DinoApp extends StatelessWidget {
+  const DinoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Dino Game",
+      home: Scaffold(
+        body: GameWidget<DinoGame>.controlled(
+          gameFactory: () {
+            return DinoGame(
+              camera: CameraComponent.withFixedResolution(
+                width: 360,
+                height: 180,
+              ),
+            );
+          },
+          loadingBuilder: (_) {
+            return const LoadingGame();
+          },
+          initialActiveOverlays: const [OverLayBuilderIds.mainMenu],
+          overlayBuilderMap: _overlayBuilderWidgets(),
+        ),
+      ),
+    );
+  }
+
+  Map<String, OverlayWidgetBuilder<DinoGame>> _overlayBuilderWidgets() {
+    return {
+      OverLayBuilderIds.hud: (_, game) {
+        return Hud(
+          firstText: "Score: 20",
+          secondText: "High: 22",
+          onPressedPauseIcon: () {},
+          lives: 4,
+        );
+      },
+      OverLayBuilderIds.mainMenu: (_, game) {
+        return MainMenu(
+          title: "Dino Game",
+          textFirstButton: "Jogar",
+          textSecondButton: "Configurações",
+          onPressedFirstButton: () {
+            game.overlays.remove(OverLayBuilderIds.mainMenu);
+            game.overlays.add(OverLayBuilderIds.hud);
+          },
+          onPressedSecondButton: () {},
+        );
+      },
+      OverLayBuilderIds.pauseMenu: (_, game) {
+        return PauseMenu(
+          title: "Score: 100",
+          textFirstButton: "Resume",
+          textSecondButton: "Restart",
+          textThirdButton: "Exit",
+          onPressedFirstButton: () {},
+          onPressedSecondButton: () {},
+          onPressedThirdButton: () {},
+        );
+      },
+      OverLayBuilderIds.settingsMenu: (_, game) {
+        return SettingsMenu(
+          firstText: "Music",
+          secondText: "Effects",
+          isActiveFirstSwitch: true,
+          isActiveSecondSwitch: false,
+          onChangedFirstSwitch: (bool value) {},
+          onChangedSecondSwitch: (bool value) {},
+          onPressedIconBack: () {},
+        );
+      },
+      OverLayBuilderIds.gameOverMenu: (_, game) {
+        return GameOverMenu(
+          title: "Game Over",
+          subtitle: "Seu Score: 20",
+          textFirstButton: "Restart",
+          textSecondButton: "Exit",
+          onPressedFirstButton: () {},
+          onPressedSecondButton: () {},
+        );
+      },
+    };
+  }
 }
--- game/lib/src/system/dino_game.dart
+++ game/lib/src/system/dino_game.dart
@@ -1,3 +1,86 @@
 import 'package:dependencies_src/dependencies_src.dart';
+import 'package:game/src/utils/assets_game.dart';
 
-class DinoGame extends FlameGame {}
+class DinoGame extends FlameGame {
+  DinoGame({
+    super.children,
+    super.world,
+    super.camera,
+  });
+
+  Vector2 get cameraVirtualSize => camera.viewport.virtualSize;
+
+  @override
+  Future<void> onLoad() async {
+    _setScreenConfig();
+    _loadGameAudios();
+    _startGameLoopAudio();
+    _loadGameImages();
+    _setGameBackground();
+    return super.onLoad();
+  }
+
+  @override
+  void update(double dt) {
+    // TODO: implement update
+    super.update(dt);
+  }
+
+  Future<void> _setGameBackground() async {
+    final images = [
+      ParallaxImageData(AssetsGame.parallaxOne),
+      ParallaxImageData(AssetsGame.parallaxTwo),
+      ParallaxImageData(AssetsGame.parallaxThree),
+      ParallaxImageData(AssetsGame.parallaxFour),
+      ParallaxImageData(AssetsGame.parallaxFive),
+      ParallaxImageData(AssetsGame.parallaxSix),
+    ];
+    final parallaxComponent = await loadParallaxComponent(
+      images,
+      baseVelocity: Vector2(10, 0),
+      velocityMultiplierDelta: Vector2(1.4, 0),
+    );
+    camera.backdrop.add(parallaxComponent);
+  }
+
+  Future<void> _setScreenConfig() async {
+    await Flame.device.fullScreen();
+    await Flame.device.setLandscape();
+  }
+
+  void _setCameraAtCenterOfTheViewport() {
+    camera.viewfinder.position = camera.viewport.virtualSize * 0.5;
+  }
+
+  Future<void> _loadGameAudios() async {
+    final audioList = [
+      AssetsGame.audioHurt,
+      AssetsGame.audioJump,
+      AssetsGame.audioLooper,
+    ];
+
+    FlameAudio.bgm.initialize();
+    await FlameAudio.audioCache.loadAll(audioList);
+  }
+
+  Future<void> _startGameLoopAudio() async {
+    FlameAudio.bgm.play(AssetsGame.audioLooper, volume: 0.4);
+  }
+
+  Future<void> _loadGameImages() async {
+    final imageList = [
+      AssetsGame.imageAngryPig,
+      AssetsGame.imageBat,
+      AssetsGame.imageDino,
+      AssetsGame.parallaxOne,
+      AssetsGame.parallaxTwo,
+      AssetsGame.parallaxThree,
+      AssetsGame.parallaxFour,
+      AssetsGame.parallaxFive,
+      AssetsGame.parallaxSix,
+      AssetsGame.imageRino,
+    ];
+    await images.loadAll(imageList);
+    _setCameraAtCenterOfTheViewport();
+  }
+}

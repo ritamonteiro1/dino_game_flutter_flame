11f21fa5158efc33d5a491448a1efdea30b0831f
m.rita.monteiro <rita.alves@mobills.com.br>
2024-02-20 21:11:54 -0300

    create OverLayBuilderIds

---
+++ game/lib/src/utils/overlay_builder_ids.dart
@@ -0,0 +1,7 @@
+abstract class OverLayBuilderIds {
+  static const hud = 'Hud';
+  static const gameOverMenu = 'GameOverMenu';
+  static const mainMenu = 'MainMenu';
+  static const pauseMenu = 'PauseMenu';
+  static const settingsMenu = 'SettingsMenu';
+}

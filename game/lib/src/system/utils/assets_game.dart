b3d3acfe3a57505cc5d909aeacc99c1862ebb212
m.rita.monteiro <rita.alves@mobills.com.br>
2024-02-20 21:56:07 -0300

    create AssetsGame

---
+++ game/lib/src/system/utils/assets_game.dart
@@ -0,0 +1,16 @@
+abstract class AssetsGame {
+  static const audioHurt = 'hurt.wav';
+  static const audioJump = 'jump.wav';
+  static const audioLooper = 'platformer_loop.wav';
+
+  static const imageAngryPig = 'angry_pig/Walk(36x30).png';
+  static const imageBat = 'bat/Flying(46x30).png';
+  static const imageDino = 'dino/DinoSprites-tard.png';
+  static const parallaxOne = 'parallax/plx-1.png';
+  static const parallaxTwo = 'parallax/plx-2.png';
+  static const parallaxThree = 'parallax/plx-3.png';
+  static const parallaxFour = 'parallax/plx-4.png';
+  static const parallaxFive = 'parallax/plx-5.png';
+  static const parallaxSix = 'parallax/plx-6.png';
+  static const imageRino = 'rino/Run(52x34).png';
+}

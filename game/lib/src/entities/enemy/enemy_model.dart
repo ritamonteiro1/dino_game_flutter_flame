6367a5666addf0505ec99764909dd797d3bd22f8
m.rita.monteiro <rita.alves@mobills.com.br>
2024-02-26 10:55:29 -0300

    replace attribute name on EnemyModel

--- game/lib/src/entities/enemy/enemy_model.dart
+++ game/lib/src/entities/enemy/enemy_model.dart
@@ -2,7 +2,7 @@ import 'package:dependencies_src/dependencies_src.dart';
 
 class EnemyModel {
   final Image image;
-  final int nFrames;
+  final int amountOfFrames;
   final double stepTime;
   final Vector2 textureSize;
   final double speedX;
@@ -10,7 +10,7 @@ class EnemyModel {
 
   const EnemyModel({
     required this.image,
-    required this.nFrames,
+    required this.amountOfFrames,
     required this.stepTime,
     required this.textureSize,
     required this.speedX,

ac2d2521499f72571ddeca8ddecb21ad64e741ed
m.rita.monteiro <rita.alves@mobills.com.br>
2024-02-27 18:35:49 -0300

    create InitConfig

---
+++ game/lib/src/utils/init_config/init_config.dart
@@ -0,0 +1,16 @@
+import 'package:dependencies_src/dependencies_src.dart';
+import 'package:flutter/material.dart';
+import 'package:game/src/entities/dino/dino_model.dart';
+
+abstract class InitConfig {
+  static Future<void> execute() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await _initHive();
+  }
+
+  static Future<void> _initHive() async {
+    await Hive.initFlutter();
+
+    Hive.registerAdapter<DinoModel>(DinoModelAdapter());
+  }
+}
